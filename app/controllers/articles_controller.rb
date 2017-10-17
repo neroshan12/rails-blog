22516399b3d037c58bcde7600218b061c8feb484
Nero Siva <neroshan12@gmail.com>
2017-10-17 14:57:02 +0100

    Add validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController  # inherits from application co
   def index
     @articles = Article.all
   end
-  
+
   def show
     @article = Article.find(params[:id]) # A couple of things to note. We use Article.find
     # to find the article we're interested in, passing in params[:id] to get the :id parameter
@@ -13,13 +13,19 @@ class ArticlesController < ApplicationController  # inherits from application co
   end
 
   def new
+    @article = Article.new
+    # With the validation now in place, when you call @article.save on an invalid
+    # article, it will return false. If you open app/controllers/articles_controller.rb
+    # again, you'll notice that we don't check the result of calling @article.save
+    # inside the create action. If @article.save fails in this situation, we need
+    # to show the form back to the user.
   end
 
   def create
     @article = Article.new(article_params) # Article.new is capitalized above, whereas most other references to articles in this guide have used lowercase. In this context, we are referring to the class named Article that is defined in app/models/article.rb. Class names in Ruby must begin with a capital letter.
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
   #   Here's what's going on: every Rails model can be initialized with its
   #   respective attributes, which are automatically mapped to the respective
   #   database columns. In the first line we do just that (remember that
@@ -27,6 +33,18 @@ class ArticlesController < ApplicationController  # inherits from application co
   #   @article.save is responsible for saving the model in the database.
   #   Finally, we redirect the user to the show action, which we'll define
   #   later.
+    else
+      render 'new'
+#       The new action is now creating a new instance variable called @article,
+#       and you'll see why that is in just a few moments.
+# Notice that inside the create action we use render instead of redirect_to
+#  when save returns false. The render method is used so that the @article object
+#     is passed back to the new template when it is rendered. This rendering is
+#     done within the same request as the form submission, whereas the redirect_to
+#     will tell the browser to issue another request.
+
+
+    end
    end
 
    private
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,14 @@
 class Article < ApplicationRecord
+  validates :title, presence: true,
+                  length: { minimum: 5 }
 end
+
+
+
+
+
+#  Article class inherits from ApplicationRecord. ApplicationRecord inherits from
+#  ActiveRecord::Base which supplies a great deal of functionality to your Rails
+#  models for free, including basic database CRUD (Create, Read, Update, Destroy)
+#  operations, data validation, as well as sophisticated search support and the
+#  ability to relate multiple models to one another.
